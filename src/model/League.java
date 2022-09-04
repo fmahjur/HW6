8e4c1cbb36b3707e5cf7bf625e92b8344b168c10
Reihaneh Mahjour <rihaneh763@gmail.com>
2022-09-04 20:20:51 +0430

    Add league interface

---
+++ src/model/League.java
@@ -0,0 +1,9 @@
+package model;
+
+public interface League {
+    public void addClub();
+    public boolean deleteClub(String name);
+    public void addPlay(String club1, String club2);
+    public void showClubInfo(String name);
+    public void displayTournamentTable();
+}
