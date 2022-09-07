6e419d5a263de98b39113a1ed8cc6f2f7651c513
Reihaneh Mahjour <rihaneh763@gmail.com>
2022-09-07 21:28:25 +0430

    Add VolleyballLeague class implements League Interface

--- src/service/FootballLeague.java
+++ src/service/FootballLeague.java
@@ -7,26 +7,26 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FootballLeague implements League {
-    static List<FootballClub> football = new ArrayList<FootballClub>();
+    static List<FootballClub> footballClubList = new ArrayList<FootballClub>();
 
     @Override
     public void addClub(String clubName) {
         FootballClub footballClub = new FootballClub(clubName);
-        football.add(footballClub);
+        footballClubList.add(footballClub);
     }
 
     public void addClub(FootballClub club, String nameOfTheClub, int numberOFWins, int numberOfDefeats, int numberOfDraws,
                         int goalsReceived, int goalsScored, int numberOfPoints, int numberOfPlayed) {
         club = new FootballClub(nameOfTheClub, numberOFWins, numberOfDefeats, numberOfDraws,
                 goalsReceived, goalsScored, numberOfPoints, numberOfPlayed);
-        football.add(club);
+        footballClubList.add(club);
     }
 
     @Override
     public boolean deleteClub(String name) {
-        for (int i = 0; i < football.size(); i++) {
-            if (football.get(i).getNameOfTheClub().equals(name)) {
-                football.remove(i);
+        for (int i = 0; i < footballClubList.size(); i++) {
+            if (footballClubList.get(i).getNameOfTheClub().equals(name)) {
+                footballClubList.remove(i);
                 return true;
             }
         }
@@ -44,28 +44,28 @@ public class FootballLeague implements League {
         else if (goalsScoredByTeam2 > goalsScoredByTeam1)
             winnerTeam = team2;
 
-        for (int i = 0; i < football.size(); i++) {
-            if (football.get(i).getNameOfTheClub().equals(team1)) {
-                football.get(i).setNumberOfPlayed(football.get(i).getNumberOfPlayed() + 1);
-                football.get(i).setGoalsScored(football.get(i).getGoalsScored() + goalsScoredByTeam1);
-                football.get(i).setGoalsReceived(football.get(i).getGoalsReceived() + goalsScoredByTeam2);
+        for (int i = 0; i < footballClubList.size(); i++) {
+            if (footballClubList.get(i).getNameOfTheClub().equals(team1)) {
+                footballClubList.get(i).setNumberOfPlayed(footballClubList.get(i).getNumberOfPlayed() + 1);
+                footballClubList.get(i).setGoalsScored(footballClubList.get(i).getGoalsScored() + goalsScoredByTeam1);
+                footballClubList.get(i).setGoalsReceived(footballClubList.get(i).getGoalsReceived() + goalsScoredByTeam2);
                 if (winnerTeam.equals(team1)) {
-                    football.get(i).setNumberOFWins(football.get(i).getNumberOFWins() + 1);
-                    football.get(i).setNumberOfPoints(football.get(i).getNumberOfPoints() + 3);
+                    footballClubList.get(i).setNumberOFWins(footballClubList.get(i).getNumberOFWins() + 1);
+                    footballClubList.get(i).setNumberOfPoints(footballClubList.get(i).getNumberOfPoints() + 3);
                 } else if (winnerTeam.equals("")) {
-                    football.get(i).setNumberOfDraws(football.get(i).getNumberOfDraws() + 1);
-                    football.get(i).setNumberOfPoints(football.get(i).getNumberOfPoints() + 3);
+                    footballClubList.get(i).setNumberOfDraws(footballClubList.get(i).getNumberOfDraws() + 1);
+                    footballClubList.get(i).setNumberOfPoints(footballClubList.get(i).getNumberOfPoints() + 3);
                 }
-            } else if (football.get(i).getNameOfTheClub().equals(team2)) {
-                football.get(i).setNumberOfPlayed(football.get(i).getNumberOfPlayed() + 1);
-                football.get(i).setGoalsScored(football.get(i).getGoalsScored() + goalsScoredByTeam2);
-                football.get(i).setGoalsReceived(football.get(i).getGoalsReceived() + goalsScoredByTeam1);
+            } else if (footballClubList.get(i).getNameOfTheClub().equals(team2)) {
+                footballClubList.get(i).setNumberOfPlayed(footballClubList.get(i).getNumberOfPlayed() + 1);
+                footballClubList.get(i).setGoalsScored(footballClubList.get(i).getGoalsScored() + goalsScoredByTeam2);
+                footballClubList.get(i).setGoalsReceived(footballClubList.get(i).getGoalsReceived() + goalsScoredByTeam1);
                 if (winnerTeam.equals(team2)) {
-                    football.get(i).setNumberOFWins(football.get(i).getNumberOFWins() + 1);
-                    football.get(i).setNumberOfPoints(football.get(i).getNumberOfPoints() + 3);
+                    footballClubList.get(i).setNumberOFWins(footballClubList.get(i).getNumberOFWins() + 1);
+                    footballClubList.get(i).setNumberOfPoints(footballClubList.get(i).getNumberOfPoints() + 3);
                 } else if (winnerTeam.equals("")) {
-                    football.get(i).setNumberOfDraws(football.get(i).getNumberOfDraws() + 1);
-                    football.get(i).setNumberOfPoints(football.get(i).getNumberOfPoints() + 3);
+                    footballClubList.get(i).setNumberOfDraws(footballClubList.get(i).getNumberOfDraws() + 1);
+                    footballClubList.get(i).setNumberOfPoints(footballClubList.get(i).getNumberOfPoints() + 3);
                 }
             }
         }
@@ -73,8 +73,8 @@ public class FootballLeague implements League {
 
 
     public void showClubsInfo() {
-        for (int i = 0; i < football.size(); i++) {
-            System.out.print(football.get(i).getNameOfTheClub() + " | ");
+        for (int i = 0; i < footballClubList.size(); i++) {
+            System.out.print(footballClubList.get(i).getNameOfTheClub() + " | ");
         }
         System.out.println("");
     }
@@ -82,15 +82,15 @@ public class FootballLeague implements League {
     @Override
     public void showClubInfo(String name) {
         ShowFootballDetailsTable tournamentDetailTable = new ShowFootballDetailsTable();
-        for (int i = 0; i < football.size(); i++) {
-            if (football.get(i).getNameOfTheClub().equals(name))
-                tournamentDetailTable.displayDetails(football.get(i));
+        for (int i = 0; i < footballClubList.size(); i++) {
+            if (footballClubList.get(i).getNameOfTheClub().equals(name))
+                tournamentDetailTable.displayDetails(footballClubList.get(i));
         }
     }
 
     @Override
     public void displayTournamentTable() {
         ShowFootballDetailsTable tournamentDetailTable = new ShowFootballDetailsTable();
-        tournamentDetailTable.displayLeagueTable(football);
+        tournamentDetailTable.displayLeagueTable(footballClubList);
     }
 }
